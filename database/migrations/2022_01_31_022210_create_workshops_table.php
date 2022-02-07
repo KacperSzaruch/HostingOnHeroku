80c42c4307dc963ca4caf22fde6e0fc863777069
KacperSzaruch <99137663+KacperSzaruch@users.noreply.github.com>
2022-02-07 13:46:38 +0100

    Add files via upload

---
+++ database/migrations/2022_01_31_022210_create_workshops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkshopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->id();
+            $table->integer("userID");
+            $table->string("title");
+            $table->string("subject");
+            $table->string("time");
+            $table->string("description");
+            $table->string("author");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+}
---
+++ database/migrations/2022_01_31_022241_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->integer("userID");
+            $table->string("question");
+            $table->string('subject');
+            $table->string('state')->deafult("not_answered");            
+            $table->string("type");
+            $table->string("author");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
